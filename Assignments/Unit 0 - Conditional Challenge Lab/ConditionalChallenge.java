fdcf72acfafff92797a904fed0cd71613ea56a88
Juan Lopez <juanlpz713@gmail.com>
2025-08-29 14:31:04 -0700

    Added ConditionalChallenge
    Worked on assignment 0.1

---
+++ Assignments/Unit 0 - Conditional Challenge Lab/ConditionalChallenge.java
@@ -0,0 +1,15 @@
+public class ConditionalChallenge {
+    public static void main(String[] args) {
+        // Your code will go here
+        
+        // Part 1: Temperature Converter
+        System.out.println("--- Part 1: Temperature Converter ---");
+        
+        // Part 2: Grade Calculator
+        
+        // Part 3: Leap Year Checker
+        
+        // Part 4: Number Classifier
+        
+    }
+}
